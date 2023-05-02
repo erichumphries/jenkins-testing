cd13e695063178a6e7d8ec4d68461c3c38cb29be
Eric Humphries <ehumphri@uno.edu>
2023-05-02 17:11:24 -0500

    Updated jsx files to fit quality check

--- express-react/react/src/components/NavBar.jsx
+++ express-react/react/src/components/NavBar.jsx
@@ -21,9 +21,9 @@ export default function NavBar ({ onHome, onNewPost }) {
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {
-                    open 
-                    ? (<XIcon className="block h-6 w-6" aria-hidden="true" />) 
-                    : (<MenuIcon className="block h-6 w-6" aria-hidden="true" />)
+                    open
+                        ? (<XIcon className="block h-6 w-6" aria-hidden="true" />)
+                        : (<MenuIcon className="block h-6 w-6" aria-hidden="true" />)
                   }
                 </Disclosure.Button>
               </div>
